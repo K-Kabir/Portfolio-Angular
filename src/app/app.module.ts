7a700b12c092a19ab4432af9584fee0c2b5246c7
Kabir <seenhubble@gmail.com>
2023-07-16 22:17:55 +0530

    sections adde

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './Components/side-bar/side-bar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { EducationComponent } from './Components/education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    SideBarComponent,
+    HomeComponent,
+    AboutComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule
